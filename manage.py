ec015bd50c3d18a83636ce0521b158122dd9504b
Abby Sawyer <sawyera@udel.edu>
2017-11-14 10:44:28 -0500

    added web form

--- manage.py
+++ manage.py
@@ -1,9 +1,13 @@
 from flask_script import Manager
 from songbase import app
 
-
 manager = Manager(app)
 
 
-if __name__ == '__main__':
+@manager.command
+def hello():
+    print "hello"
+
+
+if __name__ == "__main__":
     manager.run()
